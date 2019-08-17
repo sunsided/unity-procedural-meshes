3841077a7ab8499b1492ad4785c08b19ee7e8350
Markus Mayer <widemeadows@gmail.com>
2019-08-17 23:29:47 +0200

    Add initial road generation

---
+++ Assets/Scripts/RoadMaker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using JetBrains.Annotations;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class RoadMaker : MonoBehaviour
+{
+    public float radius = 30f;
+    public int segments = 300;
+
+    public GameObject car;
+
+    public float lineWidth = 0.3f;
+    public float roadWidth = 8f;
+    public float edgeWidth = 1f;
+    public float edgeHeight = 1f;
+
+    private MeshFilter _meshFilter;
+    private MeshCollider _meshCollider;
+
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshCollider = GetComponent<MeshCollider>();
+    }
+
+    private void Update()
+    {
+        var mb = new MeshBuilder(6);
+
+        var numberWaypoints = segments;
+        var segmentDegrees = 360f / numberWaypoints;
+        var points = new List<Vector3>();
+        for (var degrees = 0f; degrees < 360f; degrees += segmentDegrees)
+        {
+            var distance = Vector3.forward * radius;
+            var point = Quaternion.AngleAxis(degrees, Vector3.up) * distance;
+            points.Add(point);
+        }
+
+        for (var i = 1; i < points.Count + 1; ++i)
+        {
+            var pPrev = points[i - 1];
+            var p0 = points[i % points.Count];
+            var p1 = points[(i + 1) % points.Count];
+
+            ExtrudeRoad(mb, pPrev, p0, p1);
+        }
+
+        _meshFilter.mesh = mb.CreateMesh();
+        _meshCollider.sharedMesh = _meshFilter.mesh;
+    }
+
+    private void ExtrudeRoad(MeshBuilder mb, Vector3 pPrev, Vector3 p0, Vector3 p1)
+    {
+        // Line
+        var offset = Vector3.zero;
+        var target = Vector3.forward * lineWidth;
+        MakeRoadQuad(mb, pPrev, p0, p1, offset, target, 0);
+
+        // Road
+        offset = target;
+        target = Vector3.forward * roadWidth;
+        MakeRoadQuad(mb, pPrev, p0, p1, offset, target, 1);
+    }
+
+    private void MakeRoadQuad([NotNull] MeshBuilder mb, Vector3 pPrev, Vector3 p0, Vector3 p1, Vector3 offset, Vector3 targetOffset, int submesh)
+    {
+        var forward = (p1 - p0).normalized;
+
+        // Determine the orthogonal direction.
+        var perpendicular = Quaternion.LookRotation(
+            Vector3.Cross(forward, Vector3.up)
+        );
+
+        var tl = p0 + (perpendicular * offset);
+        var tr = p0 + (perpendicular * (offset + targetOffset));
+
+        var bl = p1 + (perpendicular * offset);
+        var br = p1 + (perpendicular * (offset + targetOffset));
+
+        mb.BuildTriangle(tl, tr, bl, submesh);
+        mb.BuildTriangle(tr, br, bl, submesh);
+    }
+}
